ddefb78eaf6254a4cad6ac479c7c3de02b552e8f
Banerjee <jbanerjee@medline.com>
2021-03-25 16:13:16 -0500

    Day 1 Coding

---
+++ day_1/day1.py
@@ -0,0 +1,14 @@
+
+# Output
+print('Hello World\n'*3)
+print('Hello' + ' ' + 'Angela')
+name = input('What is your name ? ')
+print("Hello",name,'!')
+
+# Input to Code
+print('Hello ' + input('What is your name ? '))
+
+# Length of Name
+name = input('What is your name ? ')
+print('Name-{}, Length-{}'.format(name, len(name)))
+
