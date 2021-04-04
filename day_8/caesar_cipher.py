6411aad4ab10cbb06656c005d295f74e3ac43cb3
JAGANNATH BANERJEE <jbanerjee@medline.com>
2021-04-04 08:53:24 -0500

    Day 8 Coding

--- day_8/caesar_cipher.py
+++ day_8/caesar_cipher.py
@@ -1,37 +1,58 @@
 # Ceasar Cipher
+import art
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-spec_char = ['~', ':', "'", '+', '[', '\\', '@', '^', '{', '%', '(', '-', '"', '*', '|', ',', '&', '<', '`', '}', '.', '_', '=', ']', '!', '>', ';', '?', '#', '$', ')', '/', ' ']
+spec_char = ['~', ':', "'", '+', '[', '\\', '@', '^', '{', '%', '(', '-', '"', '*', '|', ',', '&', '<', '`', '}', '.', '_', '=', ']', '!', '>', ';', '?', '#', '$', ')', '/', ' ', '0','1', '2' , '3', '4', '5', '6', '7', '8', '9']
 
-def encode_message(text, shift):
+def encode_decode_message(text, shift, direction):
     '''
-        Function to enncode letters with shift
+        Function to enncode with shift
     '''
     
-    encrypted_word =''
+    encrypt_decrypt_word =''
 
-    for letters in  text:
-        if letters in spec_char:
-            new_letter = letters
-        else:
-            letter_position = alphabet.index(letters)
-            
-            if letter_position + shift <= 26:
-                new_letter = alphabet[letter_position + shift]
+    if shift > 26:
+        shift = shift % 26
+
+    if direction == 'e' or direction == 'd':
+        
+        for letters in  text:
+            # Check for Special Characters 
+            if letters in spec_char:
+                new_letter = letters
             else:
-                new_letter = alphabet[letter_position + shift - 26]                                
-         
-        encrypted_word += new_letter
+                # Find the letter position
+                letter_position = alphabet.index(letters)
+                
+                # Logic for Encoding
+                if direction == 'e':
+                    if letter_position + shift < 26:
+                        new_letter = alphabet[letter_position + shift]
+                    else:
+                        new_letter = alphabet[letter_position + shift - 26]      
+                
+                # Logic for Decoding
+                else:
+                    if letter_position - shift >= 0:
+                        new_letter = alphabet[letter_position - shift ]
+                    else:
+                        new_letter = alphabet[letter_position - shift + 26 ]                             
+            
+            encrypt_decrypt_word += new_letter
+        return encrypt_decrypt_word
+    else:
+        return 'Incorrect Choice'
     
-    return encrypted_word
-
-
 
 if __name__ == '__main__':
-    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-    text = input("Type your message:\n").lower()
-    shift = int(input("Type the shift number:\n"))
-
-    if direction == 'encode':
-        print(f'Encrpted Message is : {encode_message(text, shift)}')
+    ''' Main Function '''
+    print(art.logo)
+    usr_option = 'yes'
+    while usr_option != 'no':
+        direction = input("Type 'encode (e) ' to encrypt, type 'decode (d) ' to decrypt:\n")
+        text = input("Type your message:\n").lower()
+        shift = int(input("Type the shift number:\n"))
+        print(f'You choose {direction}. Result - {encode_decode_message(text, shift, direction)}')
+        usr_option = input('Want to try again(yes/no). Type no to exit : ').lower()
+    print('Thank you for trying the Caesar Cipher!')
